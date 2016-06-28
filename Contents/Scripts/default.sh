a4d48a290b5688df9e997260807179fc716ffff7
Lukas Pustina <lukas.pustina@centerdevice.com>
2016-06-28 09:15:35 +0200

    Adds notification

--- Contents/Scripts/default.sh
+++ Contents/Scripts/default.sh
@@ -7,6 +7,8 @@ function use_report () {
     host=${1}
     now=$(date +"%Y%m%d-%H%M")
 
+    osascript -e "display notification \"Please be patient as this may take a few seconds.\" with title \"Retrieving USE report for ${host}\""
+
     temp_file=$(mktemp ${host}-${now}-XXXXX)
     output_file="${HOME}/Desktop/${temp_file}.md"
     mv ${temp_file} ${output_file}
